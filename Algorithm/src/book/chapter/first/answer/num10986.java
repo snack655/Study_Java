053c05c24bd9591545e68f541b3a9065c77fcd7f
snack655 <cmjcmj1230@gmail.com>
2022-05-19 20:51:21 +0900

    Solve : number 10986

---
+++ Algorithm/src/book/chapter/first/answer/num10986.java
@@ -0,0 +1,34 @@
+package book.chapter.first.answer;
+
+import java.util.Scanner;
+
+public class num10986 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        long[] S = new long[N];
+        long[] C = new long[M];
+
+        long answer = 0;
+        S[0] = sc.nextInt();
+        for (int i = 1; i < N; i++) {
+            S[i] = S[i - 1] + sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            int remember = (int) (S[i] % M);
+            if (remember == 0) answer++;
+            C[remember]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (C[i] > 1) {
+                answer = answer + (C[i] * (C[i] - 1) / 2);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
