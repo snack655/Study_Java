5b07295e27d2d5538a52d0f4a6b51d934d481c91
snack655 <cmjcmj1230@gmail.com>
2022-06-24 11:52:38 +0900

    Study : UDP

---
+++ TestProject/src/kr/hs/dgsw/udp/Server.java
@@ -0,0 +1,21 @@
+package kr.hs.dgsw.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket ds = new DatagramSocket(9999);
+
+        byte[] buffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+
+        while(true) {
+            ds.receive(receivePacket);
+
+            String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.print(msg + " ");
+        }
+    }
+}
