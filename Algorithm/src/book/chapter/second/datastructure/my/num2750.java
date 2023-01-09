ec39e196f9204d34276f4caff460b2d6ef0569ad
최민재 <cmjcmj1230@gmail.com>
2023-01-09 20:30:33 +0900

    Solve num2750 bubble sort

--- Algorithm/src/book/chapter/second/datastructure/my/num2750.java
+++ Algorithm/src/book/chapter/second/datastructure/my/num2750.java
@@ -1,20 +1,36 @@
 package book.chapter.second.datastructure.my;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
 
 public class num2750 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        ArrayList numList = new ArrayList();
-        for (int i = 0; i < num; i++) {
-            numList.add(scan.nextInt());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
         }
-        Collections.sort(numList);
-        for (int i = 0; i < num; i++) {
-            System.out.println(numList.get(i));
+
+        boolean hasSwap;
+        do {
+            hasSwap = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr.get(i) > arr.get(i + 1)) {
+                    hasSwap = true;
+                    int swap = arr.get(i + 1);
+                    arr.set(i + 1, arr.get(i));
+                    arr.set(i, swap);
+                }
+            }
+        } while (hasSwap);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr.get(i));
         }
     }
 }
